436cfb9fcb5b49b2f9e9232d488a6e7979440161
user <user@irvm.localdomain>
2021-05-18 18:21:14 +0200

    releases

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,5 @@
 module Main where
 
-
 import Lib
 import Data.List
 import System.IO
@@ -12,13 +11,13 @@ main :: IO ()
 main = do
 
 -- server kompilira 
-    putStrLn "Napisite komandu kojom kompilirate npr: 'gcc -Wall'"
+    putStrLn "Napisite komandu kojom kompilirate npr: 'gcc'"
     compilingCommand <- getLine
 
     putStrLn "Unesite ime programa koji zelite da kompilirate npr: '1.c '"
     fileName <- getLine 
 
--- upisati u fajl config.txt komandu za kompiliranje i folder gde su testovi
+-- upisivanje u fajl config.txt komandu za kompiliranje i folder gde su testovi
     putStrLn "Unesite ime relativnu putanju foldera u kome su test primeri npr: 'test1' "
     testExamplesFolder <- getLine
 
@@ -26,11 +25,10 @@ main = do
 --  KOMPILIRAJ
     exeCommand (compilingCommand ++" "++ fileName ++ " -o out/izvrsni")
 
-
+-- niz IO fajlova iz zadatog test foldera
     filesToExecuteWith <- filesInDir testExamplesFolder
-    print (length filesToExecuteWith)
 
-
+-- upisivanje svega potrebnog u fajl 'config.txt'
     writeFile "config.txt" (compilingCommand ++ ":" ++ testExamplesFolder)
 
 -- izvrsi program za svaki fajl iz foldera sa test primerima 
@@ -40,9 +38,3 @@ main = do
     clearFilesWithPattern "izvrsni"
 
     putStrLn "kraj"
-
-
-
--- TODO napraviti fajl config.txt koji ce da cuva nacin kompiliranja i izvrsavanja,
--- imena foldera sa testovima, imena izlaznih itd..
-
